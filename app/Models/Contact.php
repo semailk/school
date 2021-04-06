3b059e0e63d851b161ff077036860dc76740b8ea
Semailk <sem445820777>
2021-04-06 13:24:42 +0400

    last commit

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
+use Backpack\CRUD\app\Models\Traits\CrudTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Contact extends Model
 {
-    use HasFactory;
+    use HasFactory, CrudTrait;
 
     protected $fillable = [
       'first_name',
